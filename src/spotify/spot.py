a9e58c0b32b06166b33b17f56172e56fadb9fbbb
Tommy Tan <tommyytan@gmail.com>
2019-04-14 06:11:17 -0400

    finalized spotify api
    Signed-off-by: Tommy Tan <tommyytan@gmail.com>

---
+++ src/spotify/spot.py
@@ -0,0 +1,56 @@
+# 1/usr/bin/python
+import spotipy
+from spotipy import util
+
+
+def show_tracks(tracks):
+    for i, item in enumerate(tracks['items']):
+        track = item['track']
+        print("   %d %32.32s %s" % (i, track['artists'][0]['name'],
+                                    track['name']))
+
+
+def add_to_playlist(track_id, playlist_name="", create_bool=False):
+    username = "128501948"
+
+    scope = 'playlist-modify-public'
+    token = util.prompt_for_user_token(
+        username, scope, client_id="d1e6626ccc62404b98c29295a998a578", client_secret="8d48393fd316432db8b2a40c99662c25", redirect_uri="https://open.spotify.com/playlist/5yBW13vPeKHCYENcMvOirv")
+
+    # test playlist id [tommy's hardcoded]
+    playlist = "spotify:user:128501948:playlist:2dxSgUcvS26JfEngwD2Vz4"
+
+    # test adding track to playlist
+    #track_id = ["5CXmbqFE0mRXOYXgEze8q3"]
+
+    if token:
+        sp = spotipy.Spotify(auth=token)
+        sp.trace = False
+
+        # creates a playlist
+        if create_bool is True:
+            playlist = sp.user_playlist_create(user=username, name=playlist_name)
+            playlist = playlist['id']
+            results = sp.user_playlist_add_tracks(username, playlist, track_id)
+
+        #results = sp.current_user_playlists()
+        # adds track to playlist ONLY if it create playlist isn't provided
+        else:
+            results = sp.user_playlist_add_tracks(username, playlist, track_id)
+
+        ''' reads from playlist '''
+        # this gets the playlist URI
+        playlist = sp.user_playlist(username, playlist, fields="tracks,next")
+
+        counter = 0
+        for k, v in playlist.items():
+            for i in range(len(v['items'])):
+                print("\n")
+                print(v['items'][i]['track']['name'])
+                counter += 1
+
+    else:
+        print("Can't get token for", username)
+
+
+# add_to_playlist(track_id=["5CXmbqFE0mRXOYXgEze8q3"], create_bool=True, playlist_name="bruh")
